2b91cda5b70b170c4e011c9667918fa7d16411c7
AtsiGupta <atsig2@illinois.edu>
2024-11-26 15:36:08 -0600

    try

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Practice Hello
+          Learn React Practice Hello Part
         </a>
       </header>
     </div>
